c7eb713b174b8ef8879dfd424847a6d3c10942ba
Anastasis Papapanagiotou <anastasis.papapanagiotou@gmail.com>
2024-07-26 13:10:38 +0300

    If the last bin in the bin cover problem has a total value < threshold, then move each of its agents to another random pool.

--- Logic/bin_cover.py
+++ Logic/bin_cover.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# TODO: if the last pool doesn't add up to more than h, move all agents to a pool with >= h randomly
 # Bin cover using 2/3 approximation in O(nlogn)
 def bin_cover_approx(agents):
     n = len(agents)
@@ -34,4 +35,12 @@ def bin_cover_approx(agents):
             curr_sum = 0
 
     agents.sort(key=lambda agent: agent.id)
+    if curr_sum < threshold_value:
+        agents_to_move = np.where(np.array([agent.pool for agent in agents]) == curr_pool)[0]
+        pools = np.array([agent.pool for i, agent in enumerate(agents) if i == agent.pool])
+        pools = np.delete(pools, np.where(pools == curr_pool))
+        for agent_id in agents_to_move:
+            agents[agent_id].take_action(np.random.choice(pools))
+        total_pools -= 1
+
     return np.array(agents), total_pools
